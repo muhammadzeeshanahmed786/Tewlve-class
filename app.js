84341c29970b51574135d2565485d603b5015d59
Muhammad Zeeshan Ahmed <78692718+shani2103792@users.noreply.github.com>
2021-09-11 00:03:39 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,136 @@
+
+
+// let start=()=>{
+//     var mainDice = document.getElementById("show-items")
+//     var genNumbers = Math.ceil(Math.random() * 6);
+
+
+
+
+//     var dice1 = ` 
+// <div class="container">
+//     <div></div>
+//     <div></div>
+//     <div></div>
+//     <div></div>
+//     <div id="special"></div>
+//     <div></div>
+//     <div></div>
+//     <div></div>
+//     <div></div>
+// </div>`
+
+
+// var dice2 = `
+// <div class="container">
+//             <div></div>
+//             <div></div>
+//             <div id="special"></div>
+//             <div></div>
+//             <div></div>
+//             <div></div>
+//             <div id="special"></div>
+//             <div></div>
+//             <div></div>
+//         </div>`
+
+
+
+
+// var dice3 = `<div class="container">
+// <div></div>
+// <div></div>
+// <div id="special"></div>
+// <div></div>
+// <div id="special"></div>
+// <div></div>
+// <div id="special"></div>
+// <div></div>
+// <div></div>
+// </div>`
+
+
+// var dice4 = `
+// <div class="container">
+//             <div id="special"></div>
+//             <div></div>
+//             <div id="special"></div>
+//             <div></div>
+//             <div></div>
+//             <div></div>
+//             <div id="special"></div>
+//             <div></div>
+//             <div id="special"></div>
+//         </div>`
+
+
+// var dice5 = `<div class="container">
+//        <div id="special"></div>
+//        <div></div>
+//        <div id="special"></div>
+//        <div></div>
+//        <div id="special"></div>
+//        <div></div>
+//        <div id="special"></div>
+//        <div></div>
+//        <div id="special"></div>
+//    </div>`
+
+// var dice6 = `
+//    <div class="container">
+//        <div id="special"></div>
+//        <div id="special"></div>
+//        <div id="special"></div>
+//        <div></div>
+//        <div></div>
+//        <div></div>
+//        <div id="special"></div>
+//        <div id="special"></div>
+//        <div id="special"></div>
+//    </div>`
+
+
+//     if (genNumbers == 1) {
+//         mainDice.innerHTML=dice1;
+//     }
+//     else if (genNumbers ==2) {
+//         mainDice.innerHTML=dice2;
+//     }
+//     else if (genNumbers ==3) {
+//         mainDice.innerHTML=dice3;
+//     }
+//     else if (genNumbers ==4) {
+//         mainDice.innerHTML=dice4;
+//     }
+//     else if (genNumbers ==5) {
+//         mainDice.innerHTML=dice5;
+//     }
+//     else if (genNumbers ==6) {
+//         mainDice.innerHTML=dice6;
+//     }
+// }
+
+
+// console.log(genNumbers)
+
+
+// function reg(){
+    
+    
+//     var first=document.getElementById("total").value;
+//     var swc=document.getElementById("discount").value;
+
+
+//     document.write("Total Amount " + first);
+//     document.write("<br>")
+//     document.write("Discount "+ swc);
+//     document.write("<br>")
+
+//     var dis= first* swc/100;
+//     var dis2=first-dis
+//     document.write("Total Amount Pay Able " + dis2);
+
+
+// }
+
+
